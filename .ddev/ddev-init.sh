78249b5a84a6c491a68c74a735293fe348546b40
Julian Pustkuchen <Julian@Pustkuchen.com>
2021-07-07 11:20:08 +0200

    Update ddev-init.sh
    Fixed directories

--- .ddev/ddev-init.sh
+++ .ddev/ddev-init.sh
@@ -2,11 +2,11 @@
 
 ddev composer require drupal/coder
 
-ddev exec chmod +x vendor/bin/phpcs
-ddev exec chmod +x vendor/bin/phpcbf
+ddev exec chmod +x ../vendor/bin/phpcs
+ddev exec chmod +x ../vendor/bin/phpcbf
 
 # Register Drupal's code sniffer rules.
-ddev exec phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer --verbose
+ddev exec phpcs --config-set installed_paths ../vendor/drupal/coder/coder_sniffer --verbose
 
 # Make Codesniffer config file writable for ordinary users in container.
-ddev exec chmod 666 vendor/squizlabs/php_codesniffer/CodeSniffer.conf
+ddev exec chmod 666 ../vendor/squizlabs/php_codesniffer/CodeSniffer.conf
