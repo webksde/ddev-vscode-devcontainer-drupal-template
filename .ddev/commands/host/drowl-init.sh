89863d21f6c6d7a8148439f24588fdecb13ec95f
Joshua Sedler <37101709+joshsedl@users.noreply.github.com>
2021-11-18 17:39:57 +0100

    Update drowl-init.sh
    added patches

--- .ddev/commands/host/drowl-init.sh
+++ .ddev/commands/host/drowl-init.sh
@@ -43,6 +43,7 @@ ddev drush en admin_toolbar admin_toolbar_tools admin_toolbar_search examples st
 #Add "patches" and "minimum-stability" section in composer.json:
 ddev composer config extra.composer-exit-on-patch-failure true
 ddev composer config extra.enable-patching true
+ddev composer config extra.patches.drupal/core.THIS_IS_A_PLACEHOLDER "PLACEHOLDER"
 ddev composer config minimum-stability dev
 
 #Give all Project informations:
