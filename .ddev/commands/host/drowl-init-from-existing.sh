1bd7c1ba457f4c38270504c9d0b8c21d4356b917
Julian Pustkuchen <Julian@Pustkuchen.com>
2021-11-23 17:54:29 +0100

    Update drowl-init-from-existing.sh
    Removed composer packages which are part of drupal/recommended-project already to prevent composer issues.

--- .ddev/commands/host/drowl-init-from-existing.sh
+++ .ddev/commands/host/drowl-init-from-existing.sh
@@ -35,7 +35,7 @@ if [ $# -eq 0 ] ; then
     y|Y|yes|Yes|YES )
       echo "Ok! requiring development tools..."
       ddev composer require --dev drupal/core-dev drush/drush phpunit/phpunit drupal/coder phpspec/prophecy-phpunit drupal/devel drupal/devel_debug_log drupal/devel_php
-      ddev composer require composer/installers cweagans/composer-patches szeidler/composer-patches-cli drupal/admin_toolbar drupal/backup_migrate drupal/core-composer-scaffold drupal/core-project-message drupal/core-recommended drupal/examples drupal/stage_file_proxy
+      ddev composer require cweagans/composer-patches szeidler/composer-patches-cli drupal/admin_toolbar drupal/backup_migrate drupal/examples drupal/stage_file_proxy
       # PHP Codesniffer Setup:
       ddev composer require squizlabs/php_codesniffer
       # Initialize development environment tools:
