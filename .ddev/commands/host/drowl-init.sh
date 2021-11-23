74447f4c04377078b3baa6a00044fa877f63c954
Julian Pustkuchen <Julian@Pustkuchen.com>
2021-11-23 19:38:41 +0100

    Move dev dependencies to dependencies

--- .ddev/commands/host/drowl-init.sh
+++ .ddev/commands/host/drowl-init.sh
@@ -17,9 +17,11 @@ ddev composer create -y 'drupal/recommended-project'
 # Starting Drupal DDEV Containers
 ddev start
 
-ddev composer require --dev drupal/core-dev drush/drush phpunit/phpunit drupal/coder phpspec/prophecy-phpunit drupal/devel drupal/devel_debug_log drupal/devel_php
+# Add dependencies:
+ddev composer require composer/installers cweagans/composer-patches szeidler/composer-patches-cli drupal/core-composer-scaffold drupal/core-project-message drupal/core-recommended drupal/devel drupal/devel_debug_log drupal/devel_php drupal/admin_toolbar drupal/backup_migrate drupal/examples drupal/stage_file_proxy
 
-ddev composer require composer/installers cweagans/composer-patches szeidler/composer-patches-cli drupal/admin_toolbar drupal/backup_migrate drupal/core-composer-scaffold drupal/core-project-message drupal/core-recommended drupal/examples drupal/stage_file_proxy
+# Add DEV dependencies (but no modules due to their database relationship):
+ddev composer require --dev drupal/core-dev drush/drush phpunit/phpunit drupal/coder phpspec/prophecy-phpunit
 
 # PHP Codesniffer Setup:
 ddev composer require squizlabs/php_codesniffer
