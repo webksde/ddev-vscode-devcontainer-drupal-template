70276499d8a117024f049e2b78869f3674055f03
Joshua Sedler <37101709+joshsedl@users.noreply.github.com>
2021-11-24 15:56:38 +0100

    Update drowl-init.sh
    Quick fix

--- .ddev/commands/host/drowl-init.sh
+++ .ddev/commands/host/drowl-init.sh
@@ -8,8 +8,6 @@
 set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Get newest Drupal Version:
 ddev composer create -y 'drupal/recommended-project'
