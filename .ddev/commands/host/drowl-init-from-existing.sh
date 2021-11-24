cd8aa9b16a3d92b4330db3a09cc12087afa5b918
Joshua Sedler <37101709+joshsedl@users.noreply.github.com>
2021-11-24 15:57:12 +0100

    quickfix

--- .ddev/commands/host/drowl-init-from-existing.sh
+++ .ddev/commands/host/drowl-init-from-existing.sh
@@ -9,8 +9,6 @@
 set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # If there are no flags do this:
 if [ $# -eq 0 ] ; then
