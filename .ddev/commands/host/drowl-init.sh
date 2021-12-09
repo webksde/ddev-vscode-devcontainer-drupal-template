4e9cd6038a38c85c33e3a2bbda9ca5a300763a36
Joshua Sedler <37101709+joshsedl@users.noreply.github.com>
2021-12-09 09:48:53 +0100

    Fixed drowl-init .gitignore question

--- .ddev/commands/host/drowl-init.sh
+++ .ddev/commands/host/drowl-init.sh
@@ -76,17 +76,24 @@ ddev export-db $DDEV_PROJECT > ./data/sql/db-complete-dump.sql
 echo "Created full database dump under data/sql/db-complete-dump.sql"
 
 # Ask if .git should be removed:
-read -p "Would you like to delete your .git directory and .gitignore file?"$'\n' answer
-  case ${answer:0:1} in
-    y|Y|yes|Yes|YES )
-      echo "Ok, deleting your .gitignore and .git..."
-      rm -r ./.git ./.gitignore ./.gitattributes -f
-    ;;
-    * )
-      echo "I don't understand :( Exiting the script..."
-      yell() { echo "$0: $*" >&2; }
-    ;;
-  esac
+bool=1
+  while [ $bool -eq 1 ]; do
+    read -p "Would you like to delete your .git directory and .gitignore file?"$'\n' answer
+    case ${answer:0:1} in
+      y|Y|yes|Yes|YES )
+        echo "Ok, deleting your .gitignore and .git..."
+        rm -r ./.git ./.gitignore ./.gitattributes -f
+        bool=0
+      ;;
+      n|N|no|No|NO )
+        echo "Ok, I won't delete your .gitignore and .git file"
+        bool=0
+      ;;
+      * )
+        echo "I don't understand."
+      ;;
+    esac
+  done
 
 # Give all Project informations:
 ddev describe
