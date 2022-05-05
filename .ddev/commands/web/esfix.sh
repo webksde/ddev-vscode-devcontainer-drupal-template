1dbe78c3f3929d694739ef27544a92ae5ce9d736
Joshua Sedler <37101709+joshsedl@users.noreply.github.com>
2022-05-05 11:01:04 +0200

    Update esfix.sh

--- .ddev/commands/web/esfix.sh
+++ .ddev/commands/web/esfix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Description: Use esfix on a folder or file for linting javascript code based on Drupal Coding Standards.
-## Usage: eslint [path]
-## Example: "ddev eslint web/modules/contrib/js/my-js-file.js"
+## Usage: esfix [path]
+## Example: "ddev esfix web/modules/contrib/js/my-js-file.js"
 
 npx eslint --fix $*
