a8c5d908a87b3bccb9f6439c775f9d16875ae9af
Joshua Sedler <37101709+joshsedl@users.noreply.github.com>
2022-05-30 17:01:45 +0200

    Removed phpcs config set as it is not required anymore and leads to errors #2

--- .ddev/commands/host/drowl-init-from-existing.sh
+++ .ddev/commands/host/drowl-init-from-existing.sh
@@ -45,8 +45,6 @@ if [ $# -eq 0 ] ; then
         # Initialize development environment tools:
         ddev exec chmod +x vendor/bin/phpcs
         ddev exec chmod +x vendor/bin/phpcbf
-        # Register Drupal's code sniffer rules.
-        ddev exec phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer --verbose
         # Make Codesniffer config file writable for ordinary users in container:
         ddev exec chmod 666 vendor/squizlabs/php_codesniffer/CodeSniffer.conf
         # Get VSCode Settings:
