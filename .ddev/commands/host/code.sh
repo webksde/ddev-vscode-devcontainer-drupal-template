b7ae224c61d322f21bb109a30ab425cb091b9c9e
Grevil <js@webks.de>
2022-11-30 11:50:14 +0100

    changed code.sh command

--- .ddev/commands/host/code.sh
+++ .ddev/commands/host/code.sh
@@ -5,4 +5,4 @@
 ## Example: "ddev code"
 
 # Note the Numbers ar the hex Value of the Container Name we are attaching VSCode to
-code --folder-uri="vscode-remote://attached-container+646465762d646465762d7673636f64652d646576636f6e7461696e65722d64727570616c392d74656d706c6174652d776562%/var/www/html"
+code --folder-uri="vscode-remote://attached-container+646465762d646465762d7673636f64652d646576636f6e7461696e65722d64727570616c2d74656d706c6174652d776562%/var/www/html"
