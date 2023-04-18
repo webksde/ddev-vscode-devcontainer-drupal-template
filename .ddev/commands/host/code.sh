36c4d298379b9c486ecd3e526329d5ae73b7dc23
Grevil <js@webks.de>
2023-04-18 12:25:41 +0200

    Adjust code.sh to dynamically attach to web container #111

--- .ddev/commands/host/code.sh
+++ .ddev/commands/host/code.sh
@@ -5,4 +5,5 @@
 ## Example: "ddev code"
 
 # Note the Numbers ar the hex Value of the Container Name we are attaching VSCode to
-code --folder-uri="vscode-remote://attached-container+646465762d646465762d7673636f64652d646576636f6e7461696e65722d64727570616c2d74656d706c6174652d776562%/var/www/html"
+WEBSERVER_NAME=ddev-"$DDEV_SITENAME"-web
+code --folder-uri vscode-remote://attached-container+$(printf "$WEBSERVER_NAME" | xxd -p)/var/www/html
