d958c7a60433a56ae336c9168d6e5964fa961c56
Joshua Sedler <37101709+joshsedl@users.noreply.github.com>
2025-07-30 15:38:52 +0200

    Update code.sh

--- .ddev/commands/host/code.sh
+++ .ddev/commands/host/code.sh
@@ -6,5 +6,12 @@
 
 # Get the webserver container name:
 WEBSERVER_NAME=ddev-"$DDEV_SITENAME"-web
+
+# Check if container is running
+if ! docker ps --format "{{.Names}}" | grep -q "^$WEBSERVER_NAME$"; then
+    echo "Container $WEBSERVER_NAME is not running. Starting DDEV..."
+    ddev restart
+fi
+
 # Attach vscode to the webserver using a hex representation of the webserver name:
 code --folder-uri=vscode-remote://attached-container+$(printf "$WEBSERVER_NAME" | od -A n -t x1 | sed 's/ *//g' | tr -d '\n')/var/www/html
