33ba6ff4a65c245f9115b5edc3decbacee10a745
Grevil <js@webks.de>
2025-08-14 14:33:21 +0200

    Add cursor command and remove namespace resolver

---
+++ .ddev/commands/host/cursor.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+## Description: Open Cursor attached to the Web-Container of the current Project
+## Usage: cursor
+## Example: "ddev cursor"
+
+# Get the webserver container name:
+WEBSERVER_NAME=ddev-"$DDEV_SITENAME"-web
+
+# Check if container is running
+if ! docker ps --format "{{.Names}}" | grep -q "^$WEBSERVER_NAME$"; then
+    echo "Container $WEBSERVER_NAME is not running. Starting DDEV..."
+    ddev restart
+fi
+
+# Attach vscode to the webserver using a hex representation of the webserver name:
+cursor --folder-uri=vscode-remote://attached-container+$(printf "$WEBSERVER_NAME" | od -A n -t x1 | sed 's/ *//g' | tr -d '\n')/var/www/html
